9a5d35801954b9535ad82334fffa6e0018ded0ad
ljaramillocanas <ljaramillocanas@gmail.com>
2022-04-23 11:51:37 -0500

    Actualizando el proyecto

--- microservicio.py
+++ microservicio.py
@@ -1,14 +1,8 @@
-from http import server
 import json
-from ssl import SSL_ERROR_INVALID_ERROR_CODE
-from tracemalloc import start
-from urllib import response
-from wsgiref import headers
 from wsgiref.simple_server import make_server
-app = (__name__)
 
 def application (environ,start_response):
-    haeaders = [('Content-type','application/json')]
+    headers = [('Content-Type','application/json')]
     status = '200 OK'
     output = 'Hola desarrolladores TdeA estoy en el server!'
     
@@ -18,8 +12,6 @@ def application (environ,start_response):
     
     return [bytes(json.dumps(response),'utf8')]
 
-server = make_server('localhost',7000,application)
+server = make_server('localhost',8000,application)
 server.handle_request()
 
-
-app.run(debug=True)
